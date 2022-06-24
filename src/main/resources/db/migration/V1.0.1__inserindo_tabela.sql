a811c53b60ca086f8834d02ac8413e87f933350e
Suellen <suellen.barrosramos@gmail.com>
2022-06-24 17:18:45 -0400

    "Criação da Tabela intermediaria"

--- src/main/resources/db/migration/V1.0.1__inserindo_tabela.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE public.professor_aplicativo(
-	prof_id integer not null,
-	apli_id integer NOT NULL
-);
-
-ALTER TABLE public.professor_aplicativo ADD CONSTRAINT professor_aplicativo_fk0 FOREIGN KEY (prof_id) REFERENCES public.professor(prof_id);
-ALTER TABLE public.professor_aplicativo ADD CONSTRAINT professor_aplicativo_fk1 FOREIGN KEY (apli_id) REFERENCES public.aplicativo(apli_id);
